3fdcf0d982f4f383e621fb02636ba9418c45871b
Younghoon Gim <khunny7@live.com>
2021-01-20 07:54:55 +0000

    adding nodejs examples update gitignore

---
+++ nodejs/global.js
@@ -0,0 +1,4 @@
+const path = require("path");
+
+console.log(`The filename is ${path.basename(__filename)}`);
+
---
+++ nodejs/globalProcess.js
@@ -0,0 +1,17 @@
+console.log(process.pid)
+console.log(process.versions.node)
+console.log(process.argv)
+
+const grab = flag => {
+    let indexAfterFlag = process.argv.indexOf(flag) + 1;
+    return process.argv[indexAfterFlag];
+}
+
+const greeting = grab("--greeting")
+const user = grab("--user");
+
+const [,,firstName, lastName] = process.argv;
+
+console.log(`Your name is ${user} ${greeting}`);
+
+
