8c9fa54955ee4aa06c5f2ab97ed0bc4c518a6819
Younghoon Gim <khunny7@live.com>
2021-04-15 11:18:10 -0700

    discord bot

---
+++ discord-bot/cognitive/index.js
@@ -0,0 +1,42 @@
+import fetch from 'node-fetch';
+import _ from 'underscore';
+
+const intentToEntitieKeysAndResolution = {
+    'ToDo': {
+        'ToDo.TaskContent': (contentArr) => contentArr.join(','),
+        'ToDo.ListType': (contentArr) => contentArr.join(','),
+    },
+    'Weather': {
+        'geographyV2': (contentArr) => contentArr[0].value,
+        'datetimeV2': (contentArr) => contentArr[0].values[0].resolution[0].value,
+    },
+}
+
+const getIntent = (query) => {
+    return fetch(`https://westus.api.cognitive.microsoft.com/luis/prediction/v3.0/apps/b509abb9-9087-4bff-893d-279b978f0a3c/slots/production/predict?subscription-key=2cbaefdd6da04ecf82145ffe0d3edd64&verbose=true&show-all-intents=true&log=true&query=${query}`)
+        .then(res => res.json())
+        .then((result) => {
+            const topIntent = result.prediction.topIntent;
+            const topIntentDomain = topIntent.split('.')[0];
+            const score = (result.prediction.intents[topIntent].score); 
+            const entities = {};
+
+            if (topIntentDomain in intentToEntitieKeysAndResolution) {
+                const resolutionArr = intentToEntitieKeysAndResolution[topIntentDomain];
+
+                for (const [key, value] of Object.entries(resolutionArr)) {
+                    if (_.has(result.prediction.entities, key)) {
+                        entities[key] = value(result.prediction.entities[key]);
+                    }
+                }
+            }
+
+            return {
+                topIntent,
+                score,
+                entities,
+            };
+        });
+}
+
+export default getIntent;
---
+++ discord-bot/index.js
@@ -0,0 +1,57 @@
+import getIntent from './cognitive/index';
+
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+
+bot.on('ready', function (evt) {
+    console.log('Connected');
+    console.log('Logged in as: ');
+    console.log(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    console.log(message);
+
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     } else if (message.substring(0, 1) === '#') {
+         var msg = message.substring(1);
+
+         getIntent(msg).then((res) => {
+             console.log(res);
+             bot.sendMessage({
+                 to: channelID,
+                 message: `Intent: ${res.topIntent}, prediction score: ${res.score}, entities: ${JSON.stringify(res.entities)}`,
+             });
+         });
+     }
+});
